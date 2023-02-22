7bcae7f33ef4a4cbb34566e840d41738b07c3007
Gao Zheng <ba361006@gmail.com>
2023-02-22 16:05:14 +0800

    managing data

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -10,7 +11,11 @@ import { Product, products } from '../products';
 export class ProductDetailsComponent implements OnInit{
   product: Product | undefined;
   
-  constructor(private route: ActivatedRoute) { }
+  // private route -> shortcut to make the inputing route a private variable
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) { }
 
   ngOnInit() {
     // First get the product id from the current route.
@@ -19,5 +24,10 @@ export class ProductDetailsComponent implements OnInit{
   
     // Find the product that correspond with the id provided in route.
     this.product = products.find(product => product.id === productIdFromRoute);
+  }
+
+  addToCart(product: Product){
+    this.cartService.addToCart(product);
+    window.alert("Your product has been added to the cart!");
   }
 }
