5db1565b34c38cc2cbee968e0b533ac661e64873
Gao Zheng <ba361006@gmail.com>
2023-04-20 11:49:26 +0800

    fix cart unit test

--- src/app/cart/cart.component.spec.ts
+++ src/app/cart/cart.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CartComponent } from './cart.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('CartComponent', () => {
   let component: CartComponent;
@@ -8,6 +9,9 @@ describe('CartComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      // importing ReactiveFormsModule can solve this problem issued by karma
+      // Error: NG0303: Can't bind to 'formGroup' since it isn't a known property of 'form'
+      imports: [HttpClientTestingModule, ReactiveFormsModule],
       declarations: [ CartComponent ]
     })
     .compileComponents();
