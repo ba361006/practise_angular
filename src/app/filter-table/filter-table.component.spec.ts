64f2f614d432687be5d738d32c4702c789be5a60
Gao Zheng <ba361006@gmail.com>
2023-04-20 11:59:12 +0800

    fix filter-table unit test

--- src/app/filter-table/filter-table.component.spec.ts
+++ src/app/filter-table/filter-table.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TableModule } from 'primeng/table';
 import { FilterTableComponent } from './filter-table.component';
+import { FilterWidgetComponent } from '../filter-widget/filter-widget.component';
 
 describe('FilterTableComponent', () => {
   let component: FilterTableComponent;
@@ -8,7 +9,10 @@ describe('FilterTableComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FilterTableComponent ]
+      imports: [TableModule],
+      // adding FilterWidgetComponent to declarations can solve this problem issued from karma
+      // Error: NG0304: 'app-filter-widget' is not a known element 
+      declarations: [ FilterTableComponent, FilterWidgetComponent ]
     })
     .compileComponents();
 
