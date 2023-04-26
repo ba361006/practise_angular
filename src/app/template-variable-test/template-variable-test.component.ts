ea3db44ac5387b1fe90467a235409c7883bf063f
Gao Zheng <ba361006@gmail.com>
2023-04-26 16:08:55 +0800

    add template variable test

--- src/app/template-variable-test/template-variable-test.component.ts
+++ src/app/template-variable-test/template-variable-test.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-template-variable-test',
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./template-variable-test.component.css']
 })
 export class TemplateVariableTestComponent {
+  TColumns = [
+    {column: 'TColumn content 0'},
+    {column: 'TColumn content 1'},
+    {column: 'TColumn content 2'},
+  ]
+  
+  TValues = [
+    {value: 'Toyota'},
+    {value: 'Benz'},
+    {value: 'KTR',},
+  ];
 
+  columns = [
+    {column: 'column content 0'},
+    {column: 'column content 1'},
+    {column: 'column content 2'},
+  ]
+  
+  values = [
+    {value: 'Toyota'},
+    {value: 'Benz'},
+    {value: 'KTR',},
+  ];
 }
