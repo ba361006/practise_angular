a29a7edd0e13215ecf2948b2eba1c4460e01e4a9
Gao Zheng <ba361006@gmail.com>
2023-05-16 17:54:46 +0800

    first try

--- src/app/echarts-practise/echarts-practise.component.ts
+++ src/app/echarts-practise/echarts-practise.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as echarts from 'echarts';
+
 
 @Component({
   selector: 'app-echarts-practise',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./echarts-practise.component.scss']
 })
 export class EchartsPractiseComponent {
-
+  ngOnInit() {
+    const element = document.getElementById('main');
+    if (element) {
+      let chart = echarts.init(element);
+      chart.setOption({
+        title: {
+          text: 'ECharts entry example'
+        },
+        tooltip: {},
+        legend: {
+          data:['Sales']
+        },
+        xAxis: {
+          data: ["shirt","cardigan","chiffon shirt","pants","heels","socks"]
+        },
+        yAxis: {},
+        series: [{
+          name: 'Sales',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }]
+      });
+    }
+  }
 }
