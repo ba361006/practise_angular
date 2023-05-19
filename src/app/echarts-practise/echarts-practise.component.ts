a8fc3b912a1e81b4647aac73c9d791b2ba4f4f2c
Gao Zheng <ba361006@gmail.com>
2023-05-19 16:28:15 +0800

    prototype of get coordinate

--- src/app/echarts-practise/echarts-practise.component.ts
+++ src/app/echarts-practise/echarts-practise.component.ts
@@ -30,8 +30,9 @@ export class EchartsPractiseComponent {
       chart.setOption({
         graphic: {
           elements: [
-            this.echartsService.getVMGroup('VM1', 20, VMStatus.online, 50, 50),
-            this.echartsService.getVMGroup('VM2', 40, VMStatus.online, 150, 50),
+            this.echartsService.getVMGroup('VM1', 0, VMStatus.online, 50, 50),
+            this.echartsService.getVMGroup('VM2', 16, VMStatus.online, 150, 50),
+            this.echartsService.getVMGroup('VM3', 100, VMStatus.offline, 250, 50),
           ]
         }
       })
--- src/app/services/echarts-graphic/echarts-graphic.service.ts
+++ src/app/services/echarts-graphic/echarts-graphic.service.ts
@@ -6,6 +6,10 @@ export enum VMStatus{
   online = 'Online',
   offline = 'Offline',
 }
+export interface Cooridnate{
+  x: number,
+  y: number,
+}
 
 export namespace EchartsGraphic{
   export enum Colour {
@@ -109,33 +113,23 @@ class VMGroupGenerator{
     x: number,
     y: number,
   ): echarts.GraphicComponentOption {
-    const vmNameXOffset = (vmName: string): number => {
-      // 15 is when font size equals to 16px, each word would offset 3px to the left
-      return 15 - 3*(vmName.length-3);
-    }
-    const vmNameYOffset = (): number => {
-      const getDigitsInString = /\d+/;
-      const matches = EchartsGraphic.Font.header.match(getDigitsInString);
-      const fontSize = matches ? parseInt(matches[0]) : 0;
-      return Math.ceil((this.VM_BODY_Y_OFFSET - fontSize)/2);
-    }
-
+    const vmNameCoordinate = this.getVmNameCoordinate(vmName, EchartsGraphic.Font.header, {x:x, y:y})
     let VMGroup: echarts.GraphicComponentOption = {
       type: 'group',
       draggable: true,
       onclick: () => console.log('VM clicked'),
       children: [
-        this.generateVMHeaderRect(x,y,utilisationColourMapper(utilisation)),
+        this.generateVMHeaderRect(x,y ,utilisationColourMapper(utilisation)),
         this.generateVMBodyRect(x,y),
-        this.generateVMNameText(x+vmNameXOffset(vmName), y+vmNameYOffset(),vmName),
-        this.generateUtilisationText(x,y,utilisation),
+        this.generateVMNameText(vmNameCoordinate, vmName),
+        this.generateUtilisationText(x+this.utilisationXOffset(utilisation), y+this.utilisationYOffset(), utilisation),
         this.generateStatusText(x,y,status),
       ]
     }
     return VMGroup;
   };
 
-  private generateVMHeaderRect(x:number, y:number, colour: EchartsGraphic.Colour){
+  private generateVMHeaderRect(x: number, y: number, colour: EchartsGraphic.Colour){
     return this.rectGenerator.genreate(
       x, 
       y, 
@@ -155,11 +149,10 @@ class VMGroupGenerator{
     )
   }
 
-  private generateVMNameText(x:number, y:number, vmName:string){
-    console.log(x)
+  private generateVMNameText(coordinate: Cooridnate, vmName:string){
     return this.textGenerator.generate(
-      x, 
-      y, 
+      coordinate.x, 
+      coordinate.y, 
       vmName, 
       EchartsGraphic.Font.header, 
       EchartsGraphic.Colour.white,
@@ -168,8 +161,8 @@ class VMGroupGenerator{
 
   private generateUtilisationText(x:number, y:number, utilisation:number){
     return this.textGenerator.generate(
-      x + 11, 
-      y + 40, 
+      x, 
+      y, 
       utilisation.toString()+'%', 
       EchartsGraphic.Font.utilisation, 
       EchartsGraphic.Colour.black
@@ -185,6 +178,104 @@ class VMGroupGenerator{
       EchartsGraphic.Colour.black
     )
   }
+  private getVmNameCoordinate(text: string, font: EchartsGraphic.Font, coordinate: Cooridnate) {
+    const shape = this.getTextShape(text, font);
+    return {
+      x: coordinate.x + Math.round((this.VM_HEADER_WIDTH - shape.x)/2),
+      y: coordinate.y + Math.round((this.VM_BODY_Y_OFFSET - shape.y)/2),
+    } as Cooridnate
+  }
+
+  private getTextShape(text: string, font: EchartsGraphic.Font): Cooridnate{
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    if (!context) throw new Error('Canvas context is not supported.');
+    context.font = font;
+    const metrics = context.measureText(text);
+    const width = metrics.width;
+    const height = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+    return {x: width, y: height} as Cooridnate;
+  }
+
+  private utilisationXOffset = (utilisation: number): number => {
+    // 15 is when font size equals to 22px, each word should offest 7px to the left
+    return this.VM_BODY_X_OFFSET + 15 - 7*(utilisation.toString().length-1);
+  }
+
+  private utilisationYOffset = (): number => {
+    // should locate at around the 20% from the top of vm body
+    return Math.ceil(this.VM_BODY_Y_OFFSET + this.VM_BODY_HEIGHT*0.2);
+  };
+
+  private statusXOffset = (): number => {
+    const getDigitsInString = /\d+/;
+    const matches = EchartsGraphic.Font.header.match(getDigitsInString);
+    const statusFontSize = matches ? parseInt(matches[0]) : 0;
+    return this.VM_BODY_WIDTH*0.2;
+  }
+
+  // private getTextShape(text: string, font: EchartsGraphic.Font): Cooridnate{
+  //   const canvas = document.createElement('canvas');
+  //   const context = canvas.getContext('2d');
+  //   if (!context) throw new Error('Canvas context is not supported.');
+  //   context.font = font;
+  //   const metrics = context.measureText(text);
+  //   const width = metrics.width;
+  //   const height = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+  //   return {x: width, y: height} as Cooridnate;
+    // util second works
+    // const text = '100%'
+    // context.font = 'sans-serif 22px';
+    // let metrics = context.measureText(text);
+    // let width = Math.round(metrics.width);
+    // let height = Math.round(metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent);
+
+    // const utilXOffset = (value: number) => {
+    //   return this.VM_BODY_X_OFFSET + Math.round((this.VM_BODY_WIDTH - value)/2);
+    // }
+    // const utilYOffset = (value: number) => {
+    //   return Math.round((this.VM_BODY_Y_OFFSET - value)/2);
+    // }
+    // console.log(width);
+    // console.log(utilXOffset(width));
+    // console.log(utilYOffset(height));
+    // console.log('-----------------------');
+    
+    // context.font = '22px sans-serif';
+    // metrics = context.measureText(text);
+    // width = Math.round(metrics.width);
+    // height = Math.round(metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent);
+    // console.log(width);
+    // console.log(utilXOffset(width));
+    // console.log(utilYOffset(height));
+
+    // // FOr VM xOffset
+    // const text = 'VM1'
+    // context.font = 'sans-serif 16px';
+    // let metrics = context.measureText(text);
+    // let width = Math.round(metrics.width);
+    // let height = Math.round(metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent);
+
+    // const vmNameXOffset = (value: number) => {
+    //   return Math.round((this.VM_HEADER_WIDTH - value)/2);
+    // }
+    // const vmNameYOffset = (value: number) => {
+    //   return Math.round((this.VM_BODY_Y_OFFSET - value)/2);
+    // }
+    // console.log(width);
+    // console.log(vmNameXOffset(width));
+    // console.log(vmNameYOffset(height));
+    // console.log('-----------------------');
+    
+    // context.font = '16px sans-serif';
+    // metrics = context.measureText(text);
+    // width = Math.round(metrics.width);
+    // height = Math.round(metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent);
+    // console.log(width);
+    // console.log(vmNameXOffset(width));
+    // console.log(vmNameYOffset(height));
+
+  // }
 }
 
 @Injectable({
